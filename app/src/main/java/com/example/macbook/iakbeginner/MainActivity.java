9078a4e314c47bec70e37bb8de10b664521004bc
agungsupriyanto <agung.gsc@gmail.com>
2018-01-28 17:05:39 +0700

    Image with Picasso

--- app/src/main/java/com/example/macbook/iakbeginner/MainActivity.java
+++ app/src/main/java/com/example/macbook/iakbeginner/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.macbook.iakbeginner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
     public final static String USERNAME ="username";
@@ -10,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView tv_username;
     private TextView tv_password;
+    private ImageView iv_picture;
+
+    private String link_picture = "https://cdn77.sadanduseless.com/wp-content/uploads/2014/06/funny-cat1.jpg";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
 
         tv_username =findViewById(R.id.tv_username);
         tv_password =findViewById(R.id.tv_password);
+        iv_picture = findViewById(R.id.iv_picture);
+
 
         String username_intent = getIntent().getStringExtra(USERNAME);
         String password_intent = getIntent().getStringExtra(PASSWORD);
@@ -26,6 +37,19 @@ public class MainActivity extends AppCompatActivity {
 
         tv_username.setText(username_intent);
         tv_password.setText(password_intent);
+
+        Picasso.with(this)
+                .load(link_picture)
+                .into(iv_picture, new Callback() {
+
+                    public void onSuccess() {
+                        Toast.makeText(MainActivity.this, "Sukses Load", Toast.LENGTH_SHORT).show();
+                    }
+
+                    public void onError() {
+                        Toast.makeText(MainActivity.this, "Gagal", Toast.LENGTH_SHORT).show();
+                    }
+                });
 
 
     }
